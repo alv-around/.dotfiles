9723b09369b01e19c445299c13ead9e019c46f04
alv-around <alfmail@pm.me>
2024-09-18 15:08:51 +0200

    usefull CAPS_LOCK

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# interrupt if error
 set -x
 
+# latop configuration
+sudo pacman -S gnome-tweaks interception-caps2esc
+sudo stow -t /etc etc
+sudo systemctl enable --now udevmon.service
+
+# console configuration
 sudo pacman -S stow zsh neovim fzf zoxide tmux ripgrep fd zoxide ttf-victor-mono-nerd
 
-stow .
+stow --ignore etc .
 
 usermod -s /usr/bin/zsh $(whoami)
 
