708d413a39fb586fea33246823e65eddcfdf7af4
alv-around <alfmail@pm.me>
2024-09-26 19:21:02 +0200

    clear terminal buffer with cntrl-l

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,8 +2,26 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- set in "n"ormal mode <leader>pv as ":Ex"
 
 -- telescope shortcuts
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+-- clear terminal window with Cntrl-l
+vim.keymap.set("t", "<C-l><C-l>", [[<C-\><C-N>:lua ClearTerm(0)<CR>]], {})
+vim.keymap.set("t", "<C-l><C-l><C-l>", [[<C-\><C-N>:lua ClearTerm(1)<CR>]], {})
+
+function ClearTerm(reset)
+	vim.opt_local.scrollback = 1
+
+	vim.api.nvim_command("startinsert")
+	if reset == 1 then
+		vim.api.nvim_feedkeys("reset", "t", false)
+	else
+		vim.api.nvim_feedkeys("clear", "t", false)
+	end
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<cr>", true, false, true), "t", true)
+
+	vim.opt_local.scrollback = 10000
+end
