30c3744a1ca00fa89e520134e6f96025572ae7e0
alv-around <alfmail@pm.me>
2025-01-03 10:58:31 +0100

    some python change

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,5 +1,6 @@
 vim.opt.clipboard = "unnamedplus"
 
 -- LSP Server to use for Python.
--- Set to "basedpyright" to use basedpyright instead of pyright.
-vim.g.lazyvim_python_lsp = "pyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+vim.g.lazyvim_python_ruff = "ruff"
+vim.g.python3_host_prog = "~/.virtualenvs/debugpy/bin/python"
