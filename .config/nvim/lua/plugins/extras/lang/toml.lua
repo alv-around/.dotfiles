835cb9632804559044079a9167843f36661e2cd6
alv-around <alfmail@pm.me>
2025-01-10 16:29:07 +0100

    toml files with 4 spaces indent

---
+++ .config/nvim/lua/plugins/extras/lang/toml.lua
@@ -0,0 +1,18 @@
+return {
+	recommended = function()
+		return LazyVim.extras.wants({
+			ft = "toml",
+			root = "*.toml",
+		})
+	end,
+	"neovim/nvim-lspconfig",
+	opts = {
+		servers = {
+			taplo = {
+				formatting = {
+					indent_string = "    ",
+				},
+			},
+		},
+	},
+}
