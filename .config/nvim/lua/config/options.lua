836fe5c40ceb910b66d6820159098246a5cde1ba
alv-around <alfmail@pm.me>
2025-01-10 16:28:10 +0100

    remove python config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,6 +1,1 @@
 vim.opt.clipboard = "unnamedplus"
-
--- LSP Server to use for Python.
--- Set to "ruff_lsp" to use the old LSP implementation version.
-vim.g.lazyvim_python_ruff = "ruff"
-vim.g.python3_host_prog = "~/.virtualenvs/debugpy/bin/python"
