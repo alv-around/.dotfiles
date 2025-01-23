71c004b23a72630d70417d3f3d5fea6e2695d6f8
alv-around <alfmail@pm.me>
2025-01-23 12:34:43 +0100

    add cmdline auto complete

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,8 +1,32 @@
 return {
-	"hrsh7th/nvim-cmp",
-	dependencies = { "hrsh7th/cmp-emoji" },
-	---@param opts cmp.ConfigSchema
-	opts = function(_, opts)
-		table.insert(opts.sources, { name = "emoji" })
-	end,
+	{
+		"hrsh7th/cmp-cmdline",
+		config = function()
+			local cmp = require("cmp")
+			---@diagnostic disable-next-line: missing-fields
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline({
+					["<C-j>"] = {
+						c = function(fallback)
+							if cmp.visible() then
+								cmp.select_next_item()
+							else
+								fallback()
+							end
+						end,
+					},
+				}),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{
+						name = "cmdline",
+						option = {
+							ignore_cmds = { "Man", "!" },
+						},
+					},
+				}),
+			})
+		end,
+	},
 }
