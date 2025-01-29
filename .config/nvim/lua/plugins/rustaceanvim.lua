f4df92ab0f5beb1e493f44e421db54c0dd471c3f
alv-around <alfmail@pm.me>
2025-01-29 10:16:49 +0100

    ignore proc-macro panick error of ark_ff

--- .config/nvim/lua/plugins/rustaceanvim.lua
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -2,4 +2,14 @@ return {
 	"mrcjkb/rustaceanvim",
 	version = "^5", -- Recommended
 	lazy = false, -- This plugin is already lazy
+	opts = {
+		default_settings = {
+			["rust-analyzer"] = {
+				diagnostics = {
+					-- INFO: this line disable an anoying rust-analyzer error when using ark_ff
+					disable = "proc-macro-panicked",
+				},
+			},
+		},
+	},
 }
