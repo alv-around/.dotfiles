f24ac6cabea4cc7f8a37ec81393ad1f2cb5a1e87
alv-around <alfmail@pm.me>
2025-02-17 13:05:25 +0100

    add yay isntallation to script

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@
 # interrupt if error
 set -x
 
+# install yay
+sudo pacman -S --needed git base-devel stow
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
 # latop configuration
 sudo pacman -S gnome-tweaks interception-caps2esc
 sudo stow -t /etc etc
@@ -10,6 +16,7 @@ sudo systemctl enable --now udevmon.service
 
 # console configuration
 sudo pacman -S stow zsh neovim fzf zoxide tmux ripgrep fd zoxide ttf-jetbrains-mono-nerd starship
+sudo yay -S wezterm-git
 
 stow --ignore etc .
 
