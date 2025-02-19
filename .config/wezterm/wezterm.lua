dfd30d67fe2a0ddeec7f45da76a8e3ca1f4f999e
alv-around <alfmail@pm.me>
2025-02-19 12:44:45 +0100

    wezterm: alt-key to navigate panes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,20 +1,48 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This table will hold the configuration.
-local config = {}
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
+local config = wezterm.config_builder()
 
 -- Appearance
 config.color_scheme = "Tokyo Night"
 
 -- key configs
 config.leader = { key = "Space", mods = "CTRL", timeout_millisections = 2000 }
+
+-- smart splits config
+-- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
+local function is_vim(pane)
+	-- this is set by the plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+local direction_keys = {
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
 config.keys = {
 	{
 		mods = "LEADER",
@@ -28,63 +56,63 @@ config.keys = {
 	},
 	{
 		mods = "LEADER",
-		key = "p",
-		action = wezterm.action.ActivateTabRelative(-1),
-	},
-	{
-		mods = "LEADER",
-		key = "n",
-		action = wezterm.action.ActivateTabRelative(1),
-	},
-	{
-		mods = "LEADER",
-		key = "v",
+		key = ";",
 		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
 	{
 		mods = "LEADER",
-		key = "h",
+		key = "-",
 		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
 	{
-		mods = "CTRL",
+		mods = "ALT",
 		key = "h",
 		action = wezterm.action.ActivatePaneDirection("Left"),
 	},
 	{
-		mods = "CTRL",
+		mods = "ALT",
 		key = "j",
 		action = wezterm.action.ActivatePaneDirection("Down"),
 	},
 	{
-		mods = "CTRL",
+		mods = "ALT",
 		key = "k",
 		action = wezterm.action.ActivatePaneDirection("Up"),
 	},
 	{
-		mods = "CTRL",
+		mods = "ALT",
 		key = "l",
 		action = wezterm.action.ActivatePaneDirection("Right"),
 	},
 	{
-		mods = "CTRL",
+		mods = "ALT",
 		key = "LeftArrow",
 		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
 	},
 	{
-		mods = "CTRL",
+		mods = "ALT",
 		key = "RightArrow",
 		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
 	},
 	{
-		mods = "CTRL",
+		mods = "ALT",
 		key = "DownArrow",
 		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
 	},
 	{
-		mods = "CTRL",
+		mods = "ALT",
 		key = "UpArrow",
 		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+	},
+	{
+		mods = "ALT",
+		key = "p",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		mods = "ALT",
+		key = "n",
+		action = wezterm.action.ActivateTabRelative(1),
 	},
 }
 
