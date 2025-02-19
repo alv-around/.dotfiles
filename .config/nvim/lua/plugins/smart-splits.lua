7b3bc9ce1c6f1aa2bb241336df22ca19dfab9158
alv-around <alfmail@pm.me>
2025-02-19 21:42:29 +0100

    last status

--- .config/nvim/lua/plugins/smart-splits.lua
+++ .config/nvim/lua/plugins/smart-splits.lua
@@ -1,21 +1,16 @@
 return {
 	"mrjones2014/smart-splits.nvim",
 	lazy = false,
-	opts = {
-		at_edge = "wrap",
-	},
-	config = function(_, opts)
-		-- change focus window
-		vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left, { desc = "Move to left window" })
-		vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down, { desc = "Move to down window" })
-		vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up, { desc = "Move to up window" })
-		vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right, { desc = "Move to right window" })
+	config = function()
+		require("smart-splits").setup({
+			multiplexer_integration = "wezterm", -- Explicitly set for Wezterm
+			at_edge = "wrap", -- Optional: Behavior when at pane edge
+		})
 
-		-- resize window
-		vim.keymap.set("n", "<C-Left>", require("smart-splits").resize_left, { desc = "smart resize left" })
-		vim.keymap.set("n", "<C-Down>", require("smart-splits").resize_down, { desc = "smart resize down" })
-		vim.keymap.set("n", "<C-Up>", require("smart-splits").resize_up, { desc = "smart resize up" })
-		vim.keymap.set("n", "<C-Right>", require("smart-splits").resize_right, { desc = "smart resize right" })
-		require("smart-splits").setup(opts)
+		-- Navigation keymaps
+		vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+		vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+		vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+		vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
 	end,
 }
