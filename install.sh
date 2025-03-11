c27d2b3bcd40fcfa6723e588fd417a539dd9b111
alv-around <alfmail@pm.me>
2025-03-11 19:02:09 +0100

    ansible install packages

--- install.sh
+++ install.sh
@@ -3,20 +3,11 @@
 # interrupt if error
 set -x
 
-# install yay
-sudo pacman -S --needed git base-devel stow
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-
 # latop configuration
-sudo pacman -S gnome-tweaks interception-caps2esc
 sudo stow -t /etc etc
 sudo systemctl enable --now udevmon.service
 
 # console configuration
-sudo pacman -S stow zsh neovim fzf zoxide tmux ripgrep fd zoxide ttf-jetbrains-mono-nerd ttf-dejavu-nerd starship wezterm
-
 stow --ignore etc .
 
 usermod -s /usr/bin/zsh $(whoami)
