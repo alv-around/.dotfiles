727265d0b35f7373020ceb2def6a19d27f570735
alv-around <alfmail@pm.me>
2025-07-23 19:44:51 +0200

    hyprpaper: safe last wallpaper in .cache

--- home/.config/hypr/scripts/load_random_wallpaper.sh
+++ home/.config/hypr/scripts/load_random_wallpaper.sh
@@ -2,6 +2,8 @@
 
 WALLPAPER_DIRECTORY=~/wallpapers
 
+LAST_WALLPAPER_CACHE="$HOME/.cache/hyprland_last_wallpaper.txt"
+
 WALLPAPER=$(
   fd . "$WALLPAPER_DIRECTORY" -e jpg -e jpeg -e png -e gif -e bmp -e webp |
     shuf -n 1
@@ -12,3 +14,5 @@ hyprctl hyprpaper wallpaper ",$WALLPAPER"
 echo "$WALLPAPER"
 wallust run "$WALLPAPER"
 killall -SIGUSR2 waybar
+
+cp "$WALLPAPER" "$LAST_WALLPAPER_CACHE"
