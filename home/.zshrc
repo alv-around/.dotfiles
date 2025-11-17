b62ef9871a5af3d709a545982013e4e12f8a0794
alv-around <alfmail@pm.me>
2025-11-17 09:21:08 +0100

    home-manager: lazyvim config

--- home/.zshrc
+++ home/.zshrc
@@ -4,6 +4,7 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
 
+export PATH="$HOME/.nix-profile/bin:$PATH"
 export EDITOR="nvim"
 
 # starship terminal theme
@@ -120,6 +121,4 @@ eval "$(zoxide init --cmd cd zsh)"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-. "$HOME/.cargo/env"
 
--- home/default.nix
+++ home/default.nix
@@ -4,6 +4,21 @@
   nixgl,
   ...
 }:
+
+let
+  # Pin LazyVim's Version
+  lazyvim-pinned = pkgs.vimUtils.buildVimPlugin {
+    pname = "LazyVim-2025-03-01";
+    version = "2025-03-01";
+    doCheck = false;
+    src = pkgs.fetchFromGitHub {
+      owner = "LazyVim";
+      repo = "LazyVim";
+      rev = "25abbf546d564dc484cf903804661ba12de45507";
+      hash = "sha256-kgTdhFfqOK6HZrHF/LYge8cm20wuud7xlxBAUm0OL+M=";
+    };
+  };
+in
 {
   # Adjust username and homeDirectory path to your local machine
   home.username = "alv";
@@ -12,6 +27,8 @@
 
   nixGL.packages = import nixgl { inherit pkgs; };
   nixGL.defaultWrapper = "mesa";
+
+  fonts.fontconfig.enable = true;
 
   # Define the packages you want available in your user environment.
   home.packages = with pkgs; [
@@ -26,16 +43,14 @@
     wget
     zoxide
     zsh
-    # nvim
-    neovim
-    luarocks
-    lazygit
-    nixfmt-rfc-style
+    wl-clipboard
     # programming
     gcc # GNU Compiler Collection
     go # Go programming language
     rustup # Rust toolchain installer
+    # rust-analyzer
     typescript
+    lua
 
     ## TODO: install waybar & wallust
 
@@ -45,6 +60,23 @@
 
   # You can optionally add other basic Home Manager settings here,
   programs.home-manager.enable = true;
+
+  programs.lazygit.enable = true;
+
+  programs.neovim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      lazyvim-pinned
+      # rustaceanvim
+      # mason-lspconfig-nvim
+    ];
+    extraPackages = with pkgs; [
+      luarocks
+      lazygit
+      nixfmt-rfc-style
+      ripgrep
+    ];
+  };
 
   # Avoid displaying news on activation
   news.display = "silent";
