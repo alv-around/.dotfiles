acefaba658df364eaab7315eb9b8d283e9a6b91b
alv-around <alfmail@pm.me>
2025-12-02 04:41:27 +0100

    nvf: conform formatter

--- home/nvim.nix
+++ home/nvim.nix
@@ -1,6 +1,9 @@
 {
-  config, pkgs, nixgl, ...
-}:{
+  config,
+  pkgs,
+  nixgl,
+  ...
+}: {
   programs.nvf = {
     enable = true;
     settings = {
@@ -22,6 +25,11 @@
           whichKey.enable = true;
         };
 
+        tabline.nvimBufferline = {
+          enable = true;
+          mappings.closeCurrent = "<leader>bd";
+        };
+
         # use mini.statusline which is compatible with tokyonight theme
         mini.statusline.enable = true;
 
@@ -37,10 +45,26 @@
           };
         };
 
-        maps.normal = 
-          {
-            "<leader>e".action = "<cmd>Neotree toggle<CR>";
+        maps.normal = {
+          "<leader>e".action = "<cmd>Neotree toggle<CR>";
+          "<leader>f".action = ":lua require('conform').format()<CR>";
+        };
+
+        formatter.conform-nvim = {
+          enable = true;
+          setupOpts = {
+            # This is the standard Conform setup to ensure it runs on save
+            format_on_save = {
+              timeout_ms = 500;
+              lsp_fallback = true; # If no formatter is found, try LSP
+            };
+            # Explicitly map filetypes to formatters
+            formatters_by_ft = {
+              nix = ["alejandra"];
+              rust = ["rustfmt"];
+            };
           };
+        };
 
         languages = {
           enableFormat = true;
@@ -50,18 +74,30 @@
           # languages
           nix = {
             enable = true;
-            extraDiagnostics.enable = true;
             format = {
               enable = true;
-              type = "nixfmt";
+              type = "alejandra";
             };
           };
-          rust.enable = true;
+
+          rust = {
+            # handles the LSP setup internally.
+            enable = true;
+
+            # TODO: we want this but we don't want the lsp-error
+            # Ensure crate-based LSP is handled via rustaceanvim
+            # crates.enable = true;
+          };
         };
 
         lsp = {
           # Warning: uncommenting next line create error msg
           # enable = true;
+
+          # DISABLE LSP FORMATTING
+          # We want Conform to handle the save, not the native LSP client.
+          formatOnSave = false;
+
           inlayHints.enable = true;
         };
       };
