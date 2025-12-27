1e20bf20c198c15956dffbc320411bbc12218529
alv-around <8678242+alv-around@users.noreply.github.com>
2025-12-27 14:26:24 +0000

    Nvf (#49)
    * nvf: theme, languages & status bar
    
    * nvf: add neotree with which-key plugin
    
    * nvf: add sleuth and smart-splits
    
    * nvf: move nvf config to it's own file and remove lazyvim config
    
    * nvf: theme, languages & status bar
    
    * nvf: add neotree with which-key plugin
    
    * nvf: add sleuth and smart-splits
    
    * nvf: move nvf config to it's own file and remove lazyvim config
    
    * home-manager: add clipboards
    
    * nvf: conform formatter
    
    * nvf: add git buffer support and symbols
    
    * bufferline: disable numbers
    
    * nvf: lsp, diagnostic and trouble
    
    * add crates-nvim
    
    * remove conform
    
    * noice
    
    * todo-comments
    
    * Snacks picker
    
    * visual and search adjustments
    
    * aerial
    
    * blink-cmp
    
    * fix test
    
    * remove nvim config files

--- home/.config/nvim/lua/config/options.lua
+++
@@ -1,8 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
--- use native picker
-vim.g.lazyvim_picker = "snacks"
-
-vim.g.clipboard = "xclip"
