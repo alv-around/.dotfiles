18ca43e94c9dcad2b74eb3df0b4f7a18abb225c4
alv-around <alfmail@pm.me>
2026-01-30 10:59:34 +0100

    obsidian keymaps

--- home/programs/nvim/notes.nix
+++ home/programs/nvim/notes.nix
@@ -3,7 +3,6 @@ _inputs: {
     notes = {
       todo-comments.enable = true;
 
-      # TODO: watch video to extend functionality: https://www.youtube.com/watch?v=5ht8NYkU9wQ
       obsidian = {
         enable = true;
         setupOpts = {
@@ -16,24 +15,88 @@ _inputs: {
           templates = {
             folder = "1.templates";
           };
+          ## TODO: define `attachmets.img_folder` default dir where images are store
+          # attachments.img_folder = "assets/imgs";
         };
       };
     };
 
-    # TODO: see docs for more interesting keymaps: https://github.com/epwalsh/obsidian.nvim?tab=readme-ov-file#commands
     keymaps = [
+      # INFO: command docs: https://github.com/epwalsh/obsidian.nvim?tab=readme-ov-file#commands
+      # INFO: ObsidiandRename was not included as it can make big changes to vault
+      # TODO: missing ObsidianPasteImg, ObsidianToggleCheckBox, & ObsidianTOC
       {
-        key = "<leader>os";
+        key = "<leader>on";
+        mode = "n";
+        action = "<cmd>ObsidianNew<cr>";
+        desc = "New Note";
+      }
+      {
+        key = "<leader>oe";
+        mode = "n";
+        action = "<cmd>ObsidianExtractNote<cr>";
+        desc = "Create Note from selection";
+      }
+      {
+        key = "<leader>ow";
+        mode = "n";
+        action = "<cmd>ObsidianWorkspace<cr>";
+        desc = "Switch to another workspace";
+      }
+      {
+        key = "<leader>ot";
+        mode = "n";
+        action = "<cmd>ObsidianNewFromTemplate<cr>";
+        desc = "New Note from template";
+      }
+      # pickers
+      {
+        key = "<leader>o<space>";
+        mode = "n";
+        action = "<cmd>ObsidianQuickSwitch<cr>";
+        desc = "Notes Picker";
+      }
+      {
+        key = "<leader>o/";
         mode = "n";
         action = "<cmd>ObsidianSearch<cr>";
-        desc = "Search Notes";
+        desc = "Grep Notes";
       }
+      {
+        key = "<leader>oft";
+        mode = "n";
+        action = "<cmd>ObsidianTags<cr>";
+        desc = "Picker Notes by tag";
+      }
+      {
+        key = "<leader>ofl";
+        mode = "n";
+        action = "<cmd>ObsidianLinks<cr>";
+        desc = "Picker for links in current buffer";
+      }
+      {
+        key = "<leader>ofb";
+        mode = "n";
+        action = "<cmd>ObsidianBacklinks<cr>";
+        desc = "Picker for Backlinks to current buffer";
+      }
+      # obsidian links
+      {
+        key = "<leader>olo";
+        mode = "n";
+        action = "<cmd>ObsidianFollowLink<cr>";
+        desc = "Open Link in buffer";
+      }
+      # TODO: configure missing commands: ObsisidianLink, ObsidianLinkNew
+      # TODO: missing daily notes: ObsidianYesterday/Today/Tomorro ..
     ];
 
     # TODO: move whichKey to its own file
     binds.whichKey = {
       register = {
         "<leader>o" = "Obsidian";
+        "<leader>ol" = "Obsidian Links";
+        "<leader>of" = "Obsidian Pickers";
       };
     };
   };
