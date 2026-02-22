06f50703110fafba2d010e32c366451c6752eed6
alv-around <alfmail@pm.me>
2026-02-22 10:31:58 +0100

    add fa-icons to autocomplete

---
+++ home/programs/nvim/autocomplete.nix
@@ -0,0 +1,43 @@
+{pkgs, ...}: {
+  programs.nvf.settings.vim = {
+    # 1. Provide the dependency (MahanRahmati/blink-nerdfont.nvim)
+    extraPlugins = {
+      blink-nerdfont = {
+        package = pkgs.vimPlugins.blink-nerdfont-nvim;
+      };
+    };
+
+    autocomplete.blink-cmp = {
+      enable = true;
+      friendly-snippets.enable = true;
+      mappings = {
+        # complete = "<S-Tab>";
+        confirm = "<S-Tab>";
+        next = "<C-j>";
+        previous = "<C-k>";
+      };
+      setupOpts = {
+        sources = {
+          default = [
+            "lsp"
+            "path"
+            "snippets"
+            "buffer"
+            "nerdfont"
+          ];
+          providers = {
+            nerdfont = {
+              module = "blink-nerdfont";
+              name = "Nerd Fonts";
+              score_offset = 15;
+              opts = {
+                insert = true;
+                trigger = ":-)"; # Your custom trigger!
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
