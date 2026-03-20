7ba879889bbd60663fdc163215a853b83b7450db
alv-around <alfmail@pm.me>
2026-03-20 10:22:34 +0100

    notes naming function

--- home/notes.nix
+++ home/notes.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: let
   PRIV_OBSIDIAN_VAULT = "${config.home.homeDirectory}/notes/personal";
@@ -70,11 +71,33 @@ in {
               path = PRIV_OBSIDIAN_VAULT;
             }
           ];
+
           templates = {
             folder = "1.templates";
           };
+
           ## TODO: define `attachmets.img_folder` default dir where images are store
           # attachments.img_folder = "assets/imgs";
+
+          # name note as prompted, taken from: https://github.com/epwalsh/obsidian.nvim?tab=readme-ov-file#configuration-options
+          note_id_func = lib.mkLuaInline ''
+            function(title)
+              -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+              -- In this case a note with the title 'My new note' will be given an ID that looks
+              -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+              local suffix = ""
+              if title ~= nil then
+                -- If title is given, transform it into valid file name.
+                suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+              else
+                -- If title is nil, just add 4 random uppercase letters to the suffix.
+                for _ = 1, 4 do
+                  suffix = suffix .. string.char(math.random(65, 90))
+                end
+              end
+              return tostring(os.time()) .. "-" .. suffix
+            end
+          '';
         };
       };
     };
