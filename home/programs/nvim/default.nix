893f15f10410cef4e0835bf327a6c69d2f4d2c3a
alv-around <alfmail@pm.me>
2026-04-25 17:52:53 +0200

    nvimTree

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -79,18 +79,13 @@ _inputs: {
 
         # neo-tree
         filetree = {
-          neo-tree = {
+          nvimTree = {
             enable = true;
             setupOpts = {
-              git_status_async = true;
-              filesystem = {
-                filtered_items = {
-                  visible = true;
-                  hide_dotfiles = false; # Don't treat dotfiles as hidden
-                  hide_by_name = [
-                    ".git" # You might still want to hide the .git folder itself
-                  ];
-                };
+              diagnostics.enable = true;
+              update_focused_file = {
+                enable = true;
+                update_root = true;
               };
             };
           };
